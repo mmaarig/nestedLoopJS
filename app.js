3d9f9eb7126ade2f1e011801d0da3b218133ddb1
Muhammad Maarig <mmaarig@gmail.com>
2022-06-13 19:18:05 +0500

    Addition Algorithm Updated

--- app.js
+++ app.js
@@ -1,65 +1,71 @@
+let input1 = "", input2 = "";
 
-function calc() {
+function start() {
 
-    let matrix1 = [[0, 0, 0],
-                   [0, 0, 0],
-                   [0, 0, 0]];
+    let alert = document.querySelector("#alert")
+    let mInput1 = document.querySelector("#mInput1")
+    let mInput2 = document.querySelector("#mInput2")
+    let mResult = document.querySelector("#mResult")
 
-    let result = [[0, 0, 0],
-                  [0, 0, 0],
-                  [0, 0, 0]];
+    mInput1.innerHTML = "", mInput2.innerHTML = "", mResult.innerHTML = "";
 
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            matrix1[i][j] = Number(prompt(`Enter Number for Row: ${i + 1} Value: ${j + 1} For 1st Matrix`));
-        }
-    }
-    //we will add first matrix array to this 2nd matrix array
-    let matrix2 = [[0, 0, 0],
-                   [0, 0, 0],
-                   [0, 0, 0]];
 
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            matrix2[i][j] = Number(prompt(`Enter Number for Row: ${i + 1} Value: ${j + 1} For 2nd Matrix`));
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
+    input1 = +document.querySelector("#input1").value;
+    input2 = +document.querySelector("#input2").value;
+
+    if (input1 !== input2) {
+        alert(`
+        ${input1} and ${input2} are not same.
+        Please enter same Numbers in both fields.`);
+        return;
     }
 
+    if (isNaN(input1) || isNaN(input2)) {
+        alert(`
+        ${input1} and ${input2} are not Numbers
+        Please enter Numbers in both fields.`);
+        return;
+    }
 
-    // 1 Matrix 
-    document.querySelector("#v1").innerHTML = matrix1[0][0];
-    document.querySelector("#v2").innerHTML = matrix1[0][1];
-    document.querySelector("#v3").innerHTML = matrix1[0][2];
-    document.querySelector("#v4").innerHTML = matrix1[1][0];
-    document.querySelector("#v5").innerHTML = matrix1[1][1];
-    document.querySelector("#v6").innerHTML = matrix1[1][2];
-    document.querySelector("#v7").innerHTML = matrix1[2][0];
-    document.querySelector("#v8").innerHTML = matrix1[2][1];
-    document.querySelector("#v9").innerHTML = matrix1[2][2];
+    if (input1 < 2 || input2 < 2) {
+        alert(`
+        Please enter Numbers greater than 1`);
+        return;
+    }
 
-    // 2 Matrix
 
-    document.querySelector("#v10").innerHTML = matrix2[0][0];
-    document.querySelector("#v11").innerHTML = matrix2[0][1];
-    document.querySelector("#v12").innerHTML = matrix2[0][2];
-    document.querySelector("#v13").innerHTML = matrix2[1][0];
-    document.querySelector("#v14").innerHTML = matrix2[1][1];
-    document.querySelector("#v15").innerHTML = matrix2[1][2];
-    document.querySelector("#v16").innerHTML = matrix2[2][0];
-    document.querySelector("#v17").innerHTML = matrix2[2][1];
-    document.querySelector("#v18").innerHTML = matrix2[2][2];
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            console.log("Working");
+            mInput1.innerHTML +=
+                `<input type="number" required min="0" max="999" id="m1-${i}-${j}" value="0">`;
+            mInput2.innerHTML +=
+                `<input type="number" required min="0" max="999" id="m2-${i}-${j}" value="0">`;
+            mResult.innerHTML +=
+                `<input type="number" disabled required min="0" max="9999999" id="mResult-${i}-${j}" value="0">`;
+        }
 
-    // Sum of  1st Matrix & 2nd Matrix
+        mInput1.innerHTML += `<br>`;
+        mInput2.innerHTML += `<br>`;
+        mResult.innerHTML += `<br>`;
+    }
+    
+    document.querySelector("#consealedInputForm").classList.remove("hidden");
+            
+}
 
-    document.querySelector("#v19").innerHTML = result[0][0];
-    document.querySelector("#v20").innerHTML = result[0][1];
-    document.querySelector("#v21").innerHTML = result[0][2];
-    document.querySelector("#v22").innerHTML = result[1][0];
-    document.querySelector("#v23").innerHTML = result[1][1];
-    document.querySelector("#v24").innerHTML = result[1][2];
-    document.querySelector("#v25").innerHTML = result[2][0];
-    document.querySelector("#v26").innerHTML = result[2][1];
-    document.querySelector("#v27").innerHTML = result[2][2];
+
+function sum() {
+
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            console.log("Working");
+
+            document.querySelector(`#mResult-${i}-${j}`).value =
+                +document.querySelector(`#m1-${i}-${j}`).value
+                +
+                +document.querySelector(`#m2-${i}-${j}`).value;
+        }
+    }
 
 }
